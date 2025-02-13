aa7bc6b4228bd2e2f48173adaa5c5735c07f57c8
Safidy RH <safidytiavina21@gmail.com>
2025-02-13 07:19:04 +0300

    use axios instead of fetch

--- src/controller/task-slice.ts
+++ src/controller/task-slice.ts
@@ -1,5 +1,6 @@
+import axios from 'axios';
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import { Task } from '../types/task';
+import { Task } from '../types/task.ts';
 
 interface TaskState {
     tasks: Task[];
@@ -25,22 +26,29 @@ export const fetchTasks = createAsyncThunk(
         const limit = 20;
         const start = (page - 1) * limit;
 
-        const response = await fetch(
-            `https://jsonplaceholder.typicode.com/todos?_start=${start}&_limit=${limit}`
-        );
+        try {
+            const response = await axios.get(
+                `https://jsonplaceholder.typicode.com/todos`, {
+                    params: {
+                        _start: start,
+                        _limit: limit
+                    }
+                }
+            );
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch tasks');
+            const data = response.data;
+            dispatch(incrementPage()); // Incrémente la page après une requête réussie
+
+            return {
+                tasks: data,
+                hasMore: data.length === limit,
+                page
+            };
+        } catch (error) {
+            if(error instanceof Error) {
+                throw new Error('Failed to fetch tasks');
+            }
         }
-
-        const data = await response.json();
-        dispatch(incrementPage()); // Incrémente la page après une requête réussie
-
-        return {
-            tasks: data,
-            hasMore: data.length === limit,
-            page
-        };
     }
 );
 
@@ -80,14 +88,14 @@ const taskSlice = createSlice({
                 state.status = state.page === 1 ? 'loading' : 'succeeded';
             })
             .addCase(fetchTasks.fulfilled, (state, action) => {
-            state.status = 'succeeded';
-            const tasksMap = new Map(state.tasks.map(task => [task.id, task]));
-            action.payload.tasks.forEach((task : Task) => {
-                tasksMap.set(task.id, task);
-            });
-            state.tasks = Array.from(tasksMap.values());
-            state.hasMore = action.payload.hasMore;
-        })
+                state.status = 'succeeded';
+                const tasksMap = new Map(state.tasks.map(task => [task.id, task]));
+                action.payload?.tasks.forEach((task : Task) => {
+                    tasksMap.set(task.id, task);
+                });
+                state.tasks = Array.from(tasksMap.values());
+                state.hasMore = action.payload?.hasMore ?? false;
+            })
             .addCase(fetchTasks.rejected, (state, action) => {
                 state.status = 'failed';
                 state.error = action.error.message ?? 'Failed to fetch tasks';
@@ -104,4 +112,5 @@ export const {
     resetPage,
     incrementPage
 } = taskSlice.actions;
+
 export default taskSlice.reducer;
